140987313cdf303042f74846d0c3ad497804336b
tpaglia493 <tommasopaglia93@outlook.it>
2023-05-29 15:18:51 +0200

    added GetPizzas() API action to return list of PizzaModel objects

--- LaMiaPizzeria/Controllers/API/PizzaApiController.cs
+++ LaMiaPizzeria/Controllers/API/PizzaApiController.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LaMiaPizzeria.DataBase;
+using LaMiaPizzeria.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LaMiaPizzeria.Controllers.API
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class PizzaApiController : ControllerBase
     {
+
+        [HttpGet]
+        public IActionResult GetPizzas()
+        {
+            using (PizzaContext db = new())
+            {
+                List<PizzaModel> pizzaModels = db.Pizze.ToList();
+                return Ok(pizzaModels);
+            }
+
+        }
     }
 }
